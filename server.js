9c522a859de0b69dbb608f29c2e55ce7603f8436
Ernest Martey Danso <ernestmarteydanso@churchofjesuschrist.org>
2024-09-21 16:12:23 -0600

    Initial commit: Set up basic web server

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var app = express();
 
-app.use('/', require('./routes'));
+app.use('/', require('./routes/index'));
 
 app.listen(3000, () => {
     console.log('server is running on port 300');
